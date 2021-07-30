151d0a944630ed7ad64e8a5f8cfd042ade501ee4
todak-sun <tjsdydwn@gmail.com>
2021-07-30 23:12:56 +0900

    chapter04 코드 작성 시작

---
+++ chapter04/src/main/java/io/todak/study/objectstudy/chapter04/movie/Customer.java
@@ -0,0 +1,11 @@
+package io.todak.study.objectstudy.chapter04.movie;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.id = id;
+        this.name = name;
+    }
+}
